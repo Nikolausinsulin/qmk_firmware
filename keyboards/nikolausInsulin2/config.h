305f4e9da420bed6c73cd77c318c88455b421b27
Nikolausinsulin <76692911+Nikolausinsulin@users.noreply.github.com>
2021-12-17 20:12:13 +0100

    added ignoremodtapinterrupt

--- keyboards/nikolausInsulin2/config.h
+++ keyboards/nikolausInsulin2/config.h
@@ -162,6 +162,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_TERM 200
 #define TAPPING_TERM_PER_KEY
 
+#define IGNORE_MOD_TAP_INTERRUPT
+
 #define AUTO_SHIFT_TIMEOUT 150
 #define NO_AUTO_SHIFT_SPECIAL
 #define NO_AUTO_SHIFT_NUMERIC
